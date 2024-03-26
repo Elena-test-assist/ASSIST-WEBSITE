4e21311f0067d63db2c0dd1de555d5ac1d81b90b
Elena-test-assist <intern.elena.ciobanu@assist.ro>
2024-03-26 13:30:14 +0200

    Navigate through Assist webpages

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -19,6 +19,8 @@ describe('Navigating through Assist Website', ()=>{
     await expect(browser).toHaveUrl('https://site-dev.assist.ro/company')
     let classNameCompanyPage= await $('p=A solid')
     await expect(classNameCompanyPage).toHaveTextContaining('A solid track record')
+    await expect(classNameCompanyPage).toExist()
+    await expect(classNameCompanyPage).toBeDisplayed()
  
 })
 
@@ -30,6 +32,8 @@ it('Navigate to Careers webpage', async()=>{
     await expect(browser).toHaveUrl('https://site-dev.assist.ro/career')
     let classNameCareersPage= await $('p=Join us and work in a')
     await expect(classNameCareersPage).toHaveTextContaining('Join us and work in a creative & innovative environment')
+    await expect(classNameCareersPage).toExist()
+    await expect(classNameCareersPage).toBeDisplayed()
 
 })
 
@@ -40,6 +44,19 @@ it('Navigate to All jobs webpage', async()=>{
     await expect(browser).toHaveUrl('https://site-dev.assist.ro/jobs')
     let classNameAllJobsPage= await $('p=Navigate your')
     await expect(classNameAllJobsPage).toHaveTextContaining('Navigate your career towards success')
+    await expect(classNameAllJobsPage).toExist()
+    await expect(classNameAllJobsPage).toBeDisplayed()
 
 })
+it('Navigate to Blog webpage', async()=>{
+    let blogButton = await $('aria/Blog')
+    await blogButton.click()
+    await browser.pause(1000)
+    await expect(browser).toHaveUrl('https://site-dev.assist.ro/blog')
+    let classNameBlogPage= await $('p=News, insights and advices')
+    await expect(classNameBlogPage).toHaveTextContaining('News, insights and advices from our team')
+    await expect(classNameBlogPage).toExist()
+    await expect(classNameBlogPage).toBeDisplayed()
+
  })
+})
